2e05e3122438121f9f738a794f798b160762f6af
JerseySchunk <jjsprq@umsystem.edu>
2025-02-09 16:47:53 -0600

    Updated Library.java - Added the "addBook" method.

--- LibraryBookManagementSystem/src/Library/Library.java
+++ LibraryBookManagementSystem/src/Library/Library.java
@@ -10,5 +10,21 @@ public class Library {
 		this.books = new Book[5];
 		this.count = 0;
 	}
+	
+	//Adds a book to the array if the book is not null and space is available.
+	//Returns true if the book is added, false if this fails.
+	public boolean addBook(Book book) {
+		if(book == null) {
+			return false;
+		}
+		if(count < books.length) {
+			books[count] = book;
+			count++;
+			return true;
+		}
+		return false;
+	}
+	
+	
 
 }
