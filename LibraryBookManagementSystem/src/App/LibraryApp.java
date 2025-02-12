94546c900af0aa61954f496711370747bfba9fcb
Gideon <gideonmasters09876@gmail.com>
2025-02-12 10:08:15 -0800

    redoing the comments in LibraryApp

--- LibraryBookManagementSystem/src/App/LibraryApp.java
+++ LibraryBookManagementSystem/src/App/LibraryApp.java
@@ -7,28 +7,24 @@ public class LibraryApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/**
-		 * Making a Library and adding a book to it  
-		 * @param newLibrary
-		 */
+
+		//Making a new Library object
 		Library newLibrary = new Library();
 		
+		//Generated books to add to the Library
 		Book book1 = new Book("Wheel of Time", "Robert Jordan", "12353235", 00.30);
 		Book book2 = new Book("Spice and Wolf Anniversary Collectors edition", "Isuna Hasekura", "123456789", 175.00);
 		Book book3 = new Book("Kitty and the Midnight Hour", "Carrie Vaughn", "1234567893", 7.99);
-		/**
-		 * Adding Books to the new Library
-		 */
-		
+
+		//Adding Books to the Library
 		newLibrary.addBook(book1);
 		newLibrary.addBook(book2);
 		newLibrary.addBook(book3);
-		
-		/**
-		 * Searching by ISBN
-		 */
 
+		
+		//Made a reference variable called find book to search the ISBN
 		Book findBook = newLibrary.searchByISBN("123456789");
+		//A if else to see if the ISBN is in the Library object
 		if (findBook != null) {
 			System.out.println("Looking for book with ISBN: "  +findBook.getISBN());
 			System.out.println("Book found: " + findBook);
