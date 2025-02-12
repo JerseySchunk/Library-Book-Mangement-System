9a7c57d728d664eb3075fed70cd8bbcee19c3e82
Gideon <gideonmasters09876@gmail.com>
2025-02-11 20:27:22 -0800

    Added Library object and added books to it

--- LibraryBookManagementSystem/src/App/LibraryApp.java
+++ LibraryBookManagementSystem/src/App/LibraryApp.java
@@ -8,9 +8,23 @@ public class LibraryApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		/**
-		 * Making Books
+		 * Making a Library and adding a book to it  
+		 * @param newLibrary
 		 */
-		Book book1 = new Book("Test 1", "Test 2", 1235325235, 45.99);
+		Library newLibrary = new Library();
+		
+		Book book1 = new Book("Place Holder 1", "Placeholder 2", "1235325235", 45.99);
+		Book book2 = new Book("Place Holder 3", "Placeholder 4", "123456789", 19.99);
+		Book book3 = new Book("Place Holder 5", "Placeholder 6", "1234567893", 20.99);
+		
+		newLibrary.addBook(book1);
+		newLibrary.addBook(book2);
+		newLibrary.addBook(book3);
+		
+		/**
+		 * Adding Books to the new Library
+		 */
+		
 	}
 
 }
