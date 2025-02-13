49b93fef3875decd612a15528b4c2f9b86b1001a
Gideon <gideonmasters09876@gmail.com>
2025-02-12 17:49:00 -0800

    Added more prints to LibraryApp.java

--- LibraryBookManagementSystem/src/Library/Library.java
+++ LibraryBookManagementSystem/src/Library/Library.java
@@ -10,7 +10,7 @@ package Library;
 public class Library {
 	private Book[] books;
 	private int count;
-//Constructor to initalize the books array and count.
+//Constructor to initialize the books array and count.
 	public Library() {
 		this.books = new Book[5];
 		this.count = 0;
